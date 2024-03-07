81c468a4c78061c758e344bfc67dcd56f64ac4b0
kdh990315 <104114383+kdh990315@users.noreply.github.com>
2024-03-07 18:51:14 +0900

    중간커밋

--- src/components/userData/UserdataSection.jsx
+++ src/components/userData/UserdataSection.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import UserbasicData from "./Detail/UserbasicData";
 import UsermatchData from "./Detail/UsermatchData";
 import Search from "../layout/Search";
 import { division, matchType } from "../../api/basic";
 import Header from "../layout/Header";
-import IsLoading from "../layout/IsLoading";
 
 const UserdataSection = () => {
   useEffect(() => {
